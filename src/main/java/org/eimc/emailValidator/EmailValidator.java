92fdb827bc8852cebf2c78a0fc8de6cdf0aea322
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-10 13:47:41 -0500

    Modifying the EMAIL_PATTERN in the EmailValidator class to allow periods in the username.

--- src/main/java/org/eimc/emailValidator/EmailValidator.java
+++ src/main/java/org/eimc/emailValidator/EmailValidator.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class EmailValidator implements Predicate<String> {
 
-    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
 
     /**
      *      Checks if the email is non-null
