6c1c2f5545d29900e42e3ee0abb958334216c9cd
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-10 12:35:57 -0500

    Implement Given-When-Then structure in CalculatorTest class.

--- src/test/java/org/eimc/Calculator/CalculatorTest.java
+++ src/test/java/org/eimc/Calculator/CalculatorTest.java
@@ -2,6 +2,7 @@ package org.eimc.Calculator;
 
 
 import org.eimc.calculator.Calculator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 // Junit Assertions
@@ -10,29 +11,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 // AssertJ assertions
 import static org.assertj.core.api.Assertions.*;
 
+/**
+ *      Test methods follow the Arrange-Act-Assert (AAA) pattern,
+ *      commonly labeled as Given-When-Then:
+ *
+ *      1. GIVEN (Arrange): Set up the required inputs, mocks, and expected outcomes.
+ *      2. WHEN (Act): Execute the method under test..
+ *      3. THEN (Assert): Verify the result using assertions (JUnit or AssertJ).
+ */
 
 public class CalculatorTest {
 
+    private Calculator calculatorUnderTest;
+
+    @BeforeEach
+    void setUp() {
+        calculatorUnderTest = new Calculator();
+    }
 
     @Test
     void testUsingJunit_ThatCanAddTwoNumbers(){
 
-        Calculator calculator = new Calculator();
-        var result = calculator.add(3,3);
+        // 1 - GIVEN
+        var number1 = 3;
+        var number2 = 3;
+        var expectedResult = 6;
 
-        // Junit Assertion library
-        assertEquals(6, result , "The value returned by the add() method can not be negative");
+        // 2 - WHEN
+        var actualResult = calculatorUnderTest.add(number1,number2);
+
+        // 3 - THEN (Junit)
+        assertEquals(expectedResult, actualResult , "The value returned by the add() method can not be negative");
 
     }
 
     @Test
     void testUsingAssertJ_ThatCanAddTwoNumbers(){
 
-        Calculator calculator = new Calculator();
-        var result = calculator.add(3,3);
+        // 1 - GIVEN
+        var number1 = 3;
+        var number2 = 3;
+        var expectedResult = 6;
 
-        // AssertJ library
-        assertThat(result).isEqualTo(6);
+        // 2 - WHEN
+        var actualResult = calculatorUnderTest.add(number1,number2);
+
+        // 3 - THEN (AsserJ)
+        assertThat(actualResult).isEqualTo(expectedResult);
 
     }
 
