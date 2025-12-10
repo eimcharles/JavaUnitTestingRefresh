0ce00434338591d89316602f5a2fb5da7db2ac31
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-10 13:34:36 -0500

    Creating the EmailValidator class using Predicate and Regex to validate email.

---
+++ src/main/java/org/eimc/emailValidator/EmailValidator.java
@@ -0,0 +1,21 @@
+package org.eimc.emailValidator;
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+public class EmailValidator implements Predicate<String> {
+
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$");
+
+    /**
+     *      Checks if the email is non-null
+     *      and matches the required regex pattern.
+     */
+
+    @Override
+    public boolean test(String email) {
+        return email != null
+                && EMAIL_PATTERN.matcher(email).matches();
+    }
+
+}
